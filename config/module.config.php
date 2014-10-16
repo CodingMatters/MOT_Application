37e28850c0525f422ad37891524813dc4fa18827
gabbydgab <gamba@gabbydgab.com>
2014-10-16 14:32:13 +0800

    updated layout design

--- config/module.config.php
+++ config/module.config.php
@@ -46,6 +46,8 @@ return [
         'template_map'              => [
             'mot-layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
             'mot-template/footer'           => __DIR__ . '/../view/layout/partial/footer.phtml',
+            'mot-template/navbar-header'           => __DIR__ . '/../view/layout/partial/navbar-header.phtml',
+            'mot-template/navbar-sidebar'           => __DIR__ . '/../view/layout/partial/navbar-sidebar.phtml',
             'mot/index/index' => __DIR__ . '/../view/mot/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',

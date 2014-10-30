c36b500b20bbe4a1f1be643cfe1f264303419451
gabbydgab <gamba@gabbydgab.com>
2014-10-30 16:30:17 +0800

    updated schema

--- data/sql/employee.sql
+++ data/sql/employee.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE IF NOT EXISTS `mot_data`;
+
+USE `mot_data`;
+
 CREATE TABLE IF NOT EXISTS `employee` (
 	`employee_id` BIGINT(10) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	`first_name` VARCHAR(100) NOT NULL DEFAULT '',
@@ -5,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `employee` (
     `last_name` VARCHAR(50) NOT NULL DEFAULT '',
     `is_active` BOOLEAN NOT NULL DEFAULT true,
 --     `employment_status` ENUM('full-time', 'part-time','contractual') NOT NULL DEFAULT 'contractual',
-    `identification_number` VARCHAR(9) NOT NULL DEFAULT '' UNIQUE KEY,
+    `identification_number` VARCHAR(9) NOT NULL DEFAULT '',
     `email_address` VARCHAR(100) NOT NULL DEFAULT '',
     `phone_number` VARCHAR(100) NOT NULL DEFAULT '',
     `mobile_number` VARCHAR(50) NOT NULL DEFAULT '',
@@ -17,13 +21,12 @@ CREATE TABLE IF NOT EXISTS `employee` (
     `umid_crn` VARCHAR(50) NOT NULL DEFAULT '',
     `date_hired` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     INDEX (employee_id, identification_number),
-    UNIQUE KEY (first_name, middle_name, last_name)
+    UNIQUE KEY (first_name, middle_name, last_name),
+    UNIQUE KEY (identification_number)
 ) Engine=InnoDB;
 
-
-
 CREATE TABLE IF NOT EXISTS `contact_person` (
-    `employee_id` VARCHAR(9) NOT NULL DEFAULT '0000-0000',
+    `identification_number` VARCHAR(9) NOT NULL DEFAULT '',
     `first_name` VARCHAR(100) NOT NULL DEFAULT '',
     `middle_name` VARCHAR(50) NOT NULL DEFAULT '',
     `last_name` VARCHAR(50) NOT NULL DEFAULT '',
@@ -32,13 +35,8 @@ CREATE TABLE IF NOT EXISTS `contact_person` (
     `mobile_number` VARCHAR(50) NOT NULL DEFAULT '',
     `address` VARCHAR(255) NOT NULL DEFAULT '',
     `relation` VARCHAR(50) NOT NULL DEFAULT '',
-    FOREIGN KEY (employee_id)
-            REFERENCES employee (employee_id)
-            ON UPDATE CASCADE 
-            ON DELETE CASCADE
+    FOREIGN KEY (identification_number) REFERENCES employee (identification_number) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE=InnoDB;
-
-
 
 CREATE TABLE IF NOT EXISTS `job_position` (
     `job_id` INT(10) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
@@ -51,30 +49,47 @@ CREATE TABLE IF NOT EXISTS `job_position` (
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `department` (
-    `department_id` INT(10) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    `name` VARCHAR(100) NOT NULL DEFAULT '' UNIQUE KEY,
-    INDEX (department_id, name)
+    `dept_id` INT(10) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `dept_code` VARCHAR(10) NOT NULL DEFAULT '',
+    `dept_name` VARCHAR(100) NOT NULL DEFAULT '',
+    UNIQUE KEY (dept_code, dept_name),
+    INDEX dept_id (dept_id, dept_code),
+    INDEX code_name (dept_code, dept_name)
 ) ENGINE=InnoDB;
+
 
 CREATE TABLE IF NOT EXISTS `branch` (
     `branch_id` INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(100) NOT NULL DEFAULT '' UNIQUE KEY,
-    INDEX (branch_id, name)
+    `branch_code` VARCHAR(10) NOT NULL DEFAULT '', 
+    `branch_name` VARCHAR(100) NOT NULL DEFAULT '',
+    UNIQUE KEY (branch_code, branch_name),    
+    INDEX branch_id (branch_id, branch_code),
+    INDEX code_name (branch_code, branch_name)
+) ENGINE=InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS `branch_department` (
+    `bd_id` BIGINT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `branch_code` VARCHAR(20) NOT NULL DEFAULT '',
+    `dept_code` VARCHAR(10) NOT NULL DEFAULT '',
+    UNIQUE KEY (branch_code, dept_code),
+    INDEX branch_dept_id (bd_id, branch_code, dept_code),
+    FOREIGN KEY (branch_code) REFERENCES branch (branch_code) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (dept_code) REFERENCES department (dept_code) ON UPDATE CASCADE ON DELETE RESTRICT
 ) ENGINE=InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS `employee_branch_department` (
     `ebd_id` BIGINT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     `employee_id` BIGINT(10) UNSIGNED NOT NULL,
-    `identification_number` VARCHAR(9) NOT NULL DEFAULT '' UNIQUE KEY,
-    `branch_id` INT(10) UNSIGNED NOT NULL,
-    `department_id` INT(10) UNSIGNED NOT NULL,
-    UNIQUE KEY (identification_number, branch_id, department_id),
-    INDEX department_branch (department_id, branch_id),
-    INDEX branch_employees (identification_number, branch_id),
-    INDEX department_employees (identification_number, department_id),
+    `identification_number` VARCHAR(9) NOT NULL DEFAULT '',
+    `branch_code` VARCHAR(10) NOT NULL DEFAULT '', 
+    `dept_code` VARCHAR(10) NOT NULL DEFAULT '',
+    INDEX department_branch (dept_code, branch_code),
+    INDEX branch_employees (identification_number, branch_code),
+    INDEX department_employees (identification_number, dept_code),
     FOREIGN KEY (identification_number) REFERENCES employee (identification_number) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (branch_id) REFERENCES branch (branch_id) ON UPDATE CASCADE ON DELETE RESTRICT,
-    FOREIGN KEY (department_id) REFERENCES department (department_id) ON UPDATE CASCADE ON DELETE RESTRICT
+    FOREIGN KEY (branch_code) REFERENCES branch (branch_code) ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (dept_code) REFERENCES department (dept_code) ON UPDATE CASCADE ON DELETE RESTRICT,
+    UNIQUE KEY (identification_number, branch_code, dept_code)
 ) ENGINE=InnoDB;
 
